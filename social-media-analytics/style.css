6338200fbe3e5ca8d0115f35414464b21df08c85
Shreelakshmi Ramesh Hegde <126308097+Shreelax21@users.noreply.github.com>
2025-04-11 20:57:09 +0530

    Update style.css

--- social-media-analytics/style.css
+++ social-media-analytics/style.css
@@ -1,1 +1,14 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  text-align: center;
+}
 
+#analytics {
+  margin-top: 30px;
+  background-color: white;
+  padding: 20px;
+  display: inline-block;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px gray;
+}
